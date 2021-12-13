706c79676a029be3032b090f22edf67ac873d3c0
kenntohara <yuanjiandou@gmail.com>
2021-12-14 00:18:02 +0900

    カリキュラム０８－３　ブログ投稿詳細の表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,20 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Blog</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        
     </head>
     <body>
         <h1>Blog Name</h1>
-           <div class='posts'>
-               <h2 class='Title'>Title 1</h2>
-               <p class='body'>This in a sample body.</p>
-           </div>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                 <div class='post'>
+                     <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
+                     <p class='body'>{{ $post->body }}</p>
+                 </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
